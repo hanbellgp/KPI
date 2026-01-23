2928b9b074280f37794eff5f976c00915b1bfe00
C2472W <wangcheng@hanbell.cn>
2026-01-23 09:54:29 +0800

    ff
    fff

--- KPI-ejb/src/java/cn/hanbell/kpi/evaluation/ProductionQuantityZJKM.java
+++ KPI-ejb/src/java/cn/hanbell/kpi/evaluation/ProductionQuantityZJKM.java
@@ -16,7 +16,7 @@ public class ProductionQuantityZJKM extends ProductionQuantity {
         queryParams.put("facno", "E");
         queryParams.put("prono", "1");
         //顾迪华2019年6月19日提出更改生产线别01 改 AT\ORC\RT
-        queryParams.put("linecode", " IN('AT','ORC','RT') ");
+        queryParams.put("linecode", " IN('AT','ORC','RT','A9') ");
         queryParams.put("typecode", "= '01' ");
         //#ITCLS CHANGE TODO #
         queryParams.put("itcls", " in ('3W76','3W80','3W79','3Y76','3Y79','3Y80','3X76','3X79','3X80')");
